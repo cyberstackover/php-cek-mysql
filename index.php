30852702657e8d3ceb4d2542edea2bf9777b15c6
herwindra <berlianherwindra@gmail.com>
2024-10-30 03:10:51 +0700

    fixing koneksi DB Azure

--- index.php
+++ index.php
@@ -1,33 +1,34 @@
 <?php
-// Inisialisasi koneksi MySQL
-$conn = mysqli_init();
+// Inisialisasi koneksi MySQL 
+$con = mysqli_init();
 
-// Set koneksi SSL
-mysqli_ssl_set($conn, NULL, NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-
-// Lakukan koneksi menggunakan SSL
-mysqli_real_connect($conn, 'tes-server-herwin.mysql.database.azure.com', 'xokvnemxcl', 'Bismillah1410', 'tes-database-herwin', 3306, MYSQLI_CLIENT_SSL);
+// Lakukan koneksi 
+mysqli_real_connect($con, "tes-server-herwin.mysql.database.azure.com", "xokvnemxcl", "Bismillah1410", "tes-database-herwin", 3306);
 
 // Cek koneksi
 if (mysqli_connect_errno()) {
     die('Koneksi gagal ke MySQL: ' . mysqli_connect_error());
 } else {
-    echo "Koneksi berhasil ke server MySQL dengan SSL<br>";
+    echo "Koneksi berhasil ke server MySQL<br><br>";
 }
 
 // Menampilkan informasi database
 $sql = "SHOW TABLES";
-$result = mysqli_query($conn, $sql);
+$result = mysqli_query($con, $sql);
 
-if ($result) {
+if (!$result) {
+    die("Query gagal: " . mysqli_error($con)); // Menampilkan error jika query gagal
+}
+
+if (mysqli_num_rows($result) > 0) {
     echo "Daftar tabel di database:<br>";
     while ($row = mysqli_fetch_row($result)) {
         echo $row[0] . "<br>";
     }
 } else {
-    echo "Error menampilkan tabel: " . mysqli_error($conn);
+    echo "Tidak ada tabel yang ditemukan di database.";
 }
 
 // Menutup koneksi
-mysqli_close($conn);
+mysqli_close($con);
 ?>
