a61c8c01a51ba9031a736d1ad31ad91d54c77269
herwindra <berlianherwindra@gmail.com>
2024-10-30 02:09:00 +0700

    fix db connection

--- index.php
+++ index.php
@@ -1,33 +1,33 @@
 <?php
-// Konfigurasi koneksi database
-$servername = "tes-server-herwin.mysql.database.azure.com"; // Sesuaikan dengan alamat server MySQL Anda
-$username = "xokvnemxcl";    // Sesuaikan dengan username MySQL Anda
-$password = "q4hf$gSagPzpLTCG";    // Sesuaikan dengan password MySQL Anda
-$dbname = "tes-database-herwin"; // Sesuaikan dengan nama database yang ingin Anda cek
+// Inisialisasi koneksi MySQL
+$conn = mysqli_init();
 
-// Membuat koneksi
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Set koneksi SSL
+mysqli_ssl_set($conn, NULL, NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 
-// Memeriksa koneksi
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+// Lakukan koneksi menggunakan SSL
+mysqli_real_connect($conn, 'tes-server-herwin.mysql.database.azure.com', 'xokvnemxcl', 'Bismillah1410', 'tes-database-herwin', 3306, MYSQLI_CLIENT_SSL);
+
+// Cek koneksi
+if (mysqli_connect_errno()) {
+    die('Koneksi gagal ke MySQL: ' . mysqli_connect_error());
 } else {
-    echo "Koneksi berhasil ke server MySQL<br>";
+    echo "Koneksi berhasil ke server MySQL dengan SSL<br>";
 }
 
 // Menampilkan informasi database
-$sql = "SHOW TABLES FROM $dbname";
-$result = $conn->query($sql);
+$sql = "SHOW TABLES";
+$result = mysqli_query($conn, $sql);
 
-if ($result->num_rows > 0) {
-    echo "Daftar tabel di database '$dbname':<br>";
-    while ($row = $result->fetch_assoc()) {
-        echo $row["Tables_in_$dbname"] . "<br>";
+if ($result) {
+    echo "Daftar tabel di database:<br>";
+    while ($row = mysqli_fetch_row($result)) {
+        echo $row[0] . "<br>";
     }
 } else {
-    echo "Tidak ada tabel yang ditemukan di database '$dbname'.";
+    echo "Error menampilkan tabel: " . mysqli_error($conn);
 }
 
 // Menutup koneksi
-$conn->close();
+mysqli_close($conn);
 ?>
